8966affbca6e097e338b2acc97c32cb0b48bba25
MacHip3r <mac_258@hotmail.com>
2022-12-07 04:17:30 -0600

    new design and dialogs for update and delete

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -4,7 +4,7 @@
     <br />
     <template>
       <v-card color="grey lighten-4">
-        <v-toolbar dense color="primary">
+        <v-toolbar flat color="primary">
           <v-toolbar-title class="toolbar-title">
             Agregar producto
           </v-toolbar-title>
@@ -12,28 +12,26 @@
         </v-toolbar>
         <v-card-text>
           <template>
-            <v-form ref="form" v-model="valid" lazy-validation>
+            <v-form
+              ref="formNewProduct"
+              v-model="validNewProduct"
+              lazy-validation
+            >
               <v-row align="center" align-content="center" justify="center">
                 <v-col cols="5">
                   <v-row justify="center" align="center" align-content="center">
-                    <!-- <v-col cols="3" class="text-right mt-n3">
-                      <h3 class="black--text">Nombre</h3>
-                    </v-col> -->
                     <v-col cols="10">
                       <v-text-field
                         class="mb-n3"
                         v-model="newProduct.p_name"
                         :rules="[(v) => !!v || 'El nombre es obligatorio']"
-                        background-color="white"
+                        background-color="greyCustom"
                         label="Nombre"
                         flat
                         solo
                         required
                       ></v-text-field>
                     </v-col>
-                    <!-- <v-col cols="3" class="text-right mt-n6">
-                      <h3 class="black--text">Categoría</h3>
-                    </v-col> -->
                     <v-col cols="10">
                       <v-select
                         class="my-n4"
@@ -43,15 +41,12 @@
                           (v) => !!v || 'Debe seleccionar una categoria',
                         ]"
                         label="Seleccionar categoria"
-                        background-color="white"
-                        required
+                        background-color="greyCustom"
                         flat
                         solo
+                        required
                       ></v-select>
                     </v-col>
-                    <!-- <v-col cols="3" class="text-right mt-n6">
-                      <h3 class="black--text">Precio</h3>
-                    </v-col> -->
                     <v-col cols="10">
                       <v-text-field
                         class="mt-n3 mb-n6 pb-n6"
@@ -59,7 +54,7 @@
                         :rules="priceRules"
                         prefix="$"
                         label="Precio de la comida"
-                        background-color="white"
+                        background-color="greyCustom"
                         flat
                         solo
                         required
@@ -67,43 +62,37 @@
                     </v-col>
                   </v-row>
                 </v-col>
-
                 <v-col cols="5">
                   <v-row>
-                    <!-- <v-col cols="4" class="text-right mt-3">
-                      <h3 class="black--text">Descripción</h3>
-                    </v-col> -->
                     <v-col cols="10">
                       <v-textarea
                         rows="4"
                         label="Descripción"
                         v-model="newProduct.p_description"
-                        background-color="white"
+                        background-color="greyCustom"
                         class="mb-n6 pb-n6"
                         no-resize
                         flat
                         solo
+                        required
                       ></v-textarea>
                     </v-col>
                   </v-row>
                   <v-row>
-                    <!-- <v-col cols="4" class="text-right mt-3">
-                      <h3 class="black--text">Stock</h3>
-                    </v-col> -->
                     <v-col cols="10">
                       <v-text-field
                         label="Cantidad del producto"
                         v-model="newProduct.p_stock"
-                        background-color="white"
+                        background-color="greyCustom"
                         class="mb-n6 pb-n6"
                         type="number"
                         flat
                         solo
+                        required
                       ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-col>
-
                 <v-col cols="2">
                   <v-row justify="center">
                     <v-col cols="12">
@@ -120,6 +109,15 @@
                 </v-col>
               </v-row>
             </v-form>
+            <v-alert
+              :value="alertNewProduct"
+              icon="fas fa-check-circle"
+              transition="scale-transition"
+              class="mt-5"
+              type="success"
+            >
+              Se añadió un nuevo producto.
+            </v-alert>
           </template>
         </v-card-text>
       </v-card>
@@ -147,7 +145,6 @@
               </span>
             </template>
           </v-select>
-
           <v-select
             v-model="showed"
             :items="show"
@@ -158,7 +155,6 @@
             background-color="third"
           >
           </v-select>
-
           <v-text-field
             v-model="search"
             append-icon="fa fa-search"
@@ -170,7 +166,6 @@
             background-color="third"
           ></v-text-field>
         </v-toolbar>
-
         <v-data-table
           :headers="headers"
           :items="filteredProducts"
@@ -186,7 +181,6 @@
               $ {{ item.p_price }}
             </span>
           </template>
-
           <template v-slot:[`item.p_description`]="{ item }">
             <span
               class="d-inline-block text-truncate mr-n16"
@@ -195,37 +189,144 @@
               {{ item.p_description }}
             </span>
           </template>
-
           <template v-slot:[`item.actions`]="{ item }">
             <v-row align="center" align-content="center">
-              <v-col cols="2" class="mx-0 my-n5">
-                <v-icon small @click="editProduct(item)"> fas fa-pen </v-icon>
-              </v-col>
-              <v-col cols="2" class="mx-0 my-n5">
-                <v-icon small @click="openDeleteProductDialog(item)">
-                  fas fa-trash
-                </v-icon>
-              </v-col>
-              <v-col cols="2" class="mx-0 my-n5">
-                <v-switch
-                  v-model="item.p_status"
-                  color="success"
-                  false-value="i"
-                  true-value="a"
-                  dense
-                  @change="changeProductState(item)"
-                ></v-switch>
-              </v-col>
+              <v-switch
+                v-model="item.p_status"
+                class="mb-5 mr-2"
+                color="success"
+                false-value="i"
+                true-value="a"
+                inset
+                hide-details
+                @change="changeProductState(item)"
+              ></v-switch>
+              <!-- <v-icon small @click="editProduct(item)"> fas fa-pen </v-icon> -->
+              <v-chip
+                class="mr-2"
+                color="warning"
+                text-color="black"
+                @click="openUpdateProductDialog(item)"
+              >
+                <v-icon small> fas fa-pen </v-icon>
+              </v-chip>
+              <v-chip color="error" @click="openDeleteProductDialog(item)">
+                <v-icon color="white" small> fas fa-trash </v-icon>
+              </v-chip>
             </v-row>
           </template>
         </v-data-table>
       </v-card>
     </template>
 
+    <v-dialog
+      class="toolbar-subtitle"
+      v-model="updateProductDialog"
+      max-width="500"
+    >
+      <v-card>
+        <v-toolbar class="toolbar-title" color="primary">
+          <v-toolbar-title> Editar producto </v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  class="mt-2"
+                  v-model="actualProduct.p_name"
+                  :rules="[(v) => !!v || 'El nombre es obligatorio']"
+                  label="Nombre"
+                  background-color="greyCustom"
+                  ref="updateProductNameInput"
+                  flat
+                  solo
+                  hide-details
+                  required
+                ></v-text-field>
+                <v-select
+                  class="mt-2"
+                  v-model="actualProduct.id_category"
+                  :items="categories"
+                  :rules="[(v) => !!v || 'Debe seleccionar una categoria']"
+                  label="Seleccionar categoria"
+                  background-color="greyCustom"
+                  flat
+                  solo
+                  hide-details
+                  required
+                ></v-select>
+                <v-text-field
+                  class="mt-2"
+                  v-model="actualProduct.p_price"
+                  :rules="priceRules"
+                  prefix="$"
+                  label="Precio de la comida"
+                  background-color="greyCustom"
+                  flat
+                  solo
+                  hide-details
+                  required
+                ></v-text-field>
+                <v-textarea
+                  class="mt-2"
+                  rows="4"
+                  label="Descripción"
+                  v-model="actualProduct.p_description"
+                  background-color="greyCustom"
+                  no-resize
+                  flat
+                  solo
+                  hide-details
+                  required
+                ></v-textarea>
+                <v-text-field
+                  class="mt-2"
+                  label="Cantidad del producto"
+                  v-model="actualProduct.p_stock"
+                  background-color="greyCustom"
+                  type="number"
+                  @keydown.enter="updateProduct()"
+                  flat
+                  solo
+                  hide-details
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-alert
+              :value="alertEmptyProduct"
+              icon="fas fa-exclamation-circle"
+              transition="scale-transition"
+              class="mt-2"
+              type="error"
+            >
+              Debes de poner valores correctos o completos.
+            </v-alert>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary darken-1"
+            text
+            @click="closeUpdateProductDialog()"
+          >
+            Cancelar
+          </v-btn>
+          <v-btn color="accent darken-1" text @click="updateProduct()">
+            Editar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-dialog v-model="deleteProductDialog" max-width="300">
       <v-card>
-        <v-card-title class="text-h5"> ¿Estás seguro? </v-card-title>
-        <v-card-text> Esta acción es irreversible. </v-card-text>
+        <v-card-title class="card-title-custom"> ¿Estás seguro? </v-card-title>
+        <v-card-text class="card-subtitle-custom">
+          Esta acción es irreversible.
+        </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
@@ -249,10 +350,13 @@ export default {
   name: "Product",
 
   data: () => ({
-    valid: false,
+    validNewProduct: false,
     dialog: false,
     updateMode: false,
     deleteProductDialog: false,
+    updateProductDialog: false,
+    alertEmptyProduct: false,
+    alertNewProduct: false,
     actualProduct: {},
     priceRules: [
       (v) =>
@@ -280,6 +384,20 @@ export default {
     showed: "all",
     newProduct: {},
   }),
+
+  watch: {
+    alertEmptyProduct(value) {
+      if (!value) return;
+
+      setTimeout(() => (this.alertEmptyProduct = false), 3000);
+    },
+
+    alertNewProduct(value) {
+      if (!value) return;
+
+      setTimeout(() => (this.alertNewProduct = false), 3000);
+    },
+  },
 
   created() {
     this.getAllProducts();
@@ -313,17 +431,41 @@ export default {
 
     async addProduct() {
       await this.axios.post("product/addProduct", this.newProduct);
+
       this.getAllProducts();
+
       this.newProduct = {};
-      this.$refs.form.reset();
+
+      this.alertNewProduct = true;
+
+      this.$refs.formNewProduct.reset();
     },
 
     async updateProduct() {
-      await this.axios.post("product/updateProduct", this.newProduct);
-      this.getAllProducts();
-      this.newProduct = {};
-      this.updateMode = false;
-      this.$refs.form.reset();
+      /* await this.axios.post("product/updateProduct", this.newProduct); */
+      if (
+        this.actualProduct.id_product == "" ||
+        this.actualProduct.id_product == 0 ||
+        this.actualProduct.id_category == "" ||
+        this.actualProduct.id_category == 0 ||
+        this.actualProduct.p_name == "" ||
+        this.actualProduct.p_price == "" ||
+        this.actualProduct.p_price == 0 ||
+        this.actualProduct.p_description == "" ||
+        this.actualProduct.p_stock == ""
+      )
+        this.alertEmptyProduct = true;
+      else {
+        await this.axios.post("product/updateProduct", this.actualProduct);
+
+        this.getAllProducts();
+        /* this.newProduct = {};
+      this.updateMode = false; */
+        this.actualProduct = {};
+        this.alertEmptyProduct = false;
+        /* this.$refs.form.reset(); */
+        this.closeUpdateProductDialog();
+      }
     },
 
     async changeProductState(item) {
@@ -336,24 +478,43 @@ export default {
       this.getAllProducts();
     },
 
-    editProduct(item) {
+    /* editProduct(item) {
       this.updateMode = true;
       this.newProduct = item;
-    },
+    }, */
 
-    cancelEditProduct() {
+    /* cancelEditProduct() {
       this.updateMode = false;
       this.newProduct = {};
       this.$refs.form.reset();
-    },
+    }, */
 
     submitForm() {
-      if (this.$refs.form.validate())
+      if (this.$refs.formNewProduct.validate())
         this.updateMode ? this.updateProduct() : this.addProduct();
     },
 
-    openDeleteProductDialog(item) {
-      this.actualProduct = item;
+    openUpdateProductDialog(product) {
+      this.actualProduct = {
+        id_product: product.id_product,
+        id_category: product.id_category,
+        p_name: product.p_name,
+        p_price: product.p_price,
+        p_description: product.p_description,
+        p_stock: product.p_stock,
+      };
+
+      this.updateProductDialog = true;
+      setTimeout(() => this.$refs.updateProductNameInput.focus(), 250);
+    },
+
+    closeUpdateProductDialog() {
+      this.actualProduct = {};
+      this.updateProductDialog = false;
+    },
+
+    openDeleteProductDialog(product) {
+      this.actualProduct = product;
       this.deleteProductDialog = true;
     },
 
